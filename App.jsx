f06d970f12860d489c7cdf837bf1b07d1209d833
fibha-dev <fibha2002@gmail.com>
2025-12-06 01:55:55 +0500

    Conditionally render Navbar based on route
    Added userprofile to the app.jsx

--- App.jsx
+++ App.jsx
@@ -1,24 +1,29 @@
 import React, { useState } from 'react';
 import Navbar from './Components/Navbar/Navbar';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Video from './Pages/Video/Video';
 import Login from './Pages/Login';
 import CreatorDashboard from './Pages/CreatorDashboard';
+import UserProfile from './Pages/UserProfile';
 
 
 const App = () => {
   const [sidebar, setSidebar] = useState(true);
+  const location = useLocation(); 
 
   return (
     <div>
-      <Navbar setSidebar={setSidebar} />
+
+      {location.pathname !== "/" && <Navbar setSidebar={setSidebar} />}
 
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home sidebar={sidebar} />} />
         <Route path="/video/:categoryId/:videoId" element={<Video sidebar={sidebar} />} />
         <Route path="/creator-dashboard" element={<CreatorDashboard />} />
+        <Route path="/profile" element={<UserProfile />} />
+
       </Routes>
     </div>
   );
